b8b95aa305a692ef677f4c5e44464c0a3148c25e
mshtakafridi488 <mshtakafridi488@gmail.com>
2025-03-05 20:06:47 +0000

    Add 1.c

---
+++ src/1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int age = 25;
+  if (age >= 18) {
+    printf("You are eligible to vote.");
+  } else {
+    printf("Sorry, you are not eligible to vote.");
+  }
+  return 0;
+}
