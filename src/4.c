baf6dafbb7a00d45bc8919f5ba57c37467f0a717
mshtakafridi488 <mshtakafridi488@gmail.com>
2025-03-08 08:55:25 +0000

    Add 4.c

---
+++ src/4.c
@@ -0,0 +1,12 @@
+// Function to calculate the sum of two numbers
+int sum(int num1, int num2) {
+  return num1 + num2;
+}
+
+// Driver program to test the above function
+int main() {
+  int num1 = 5;
+  int num2 = 8;
+  printf("The sum of %d and %d is %d", num1, num2, sum(num1, num2));
+  return 0;
+}
